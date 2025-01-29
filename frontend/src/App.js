fb2878038aa35b7fe6e382836c7c83fefd4fccc0
AMAKARATEBASAKI <cemm1240052@gn.iwasaki.ac.jp>
2025-01-29 11:33:53 +0900

    Headerのコンポーネント

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
 import jaLocale from '@fullcalendar/core/locales/ja';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
     const [transactions, setTransactions] = useState([]);
@@ -50,24 +51,7 @@ function App() {
 
     return (
         <div>
-            <div className={"header"}>
-                <button><a href={""}>ホーム</a></button>
-                <select name={"月選択"} className={"month"}>
-                    <option value={"1"}>1月</option>
-                    <option value={"2"}>2月</option>
-                    <option value={"3"}>3月</option>
-                    <option value={"4"}>4月</option>
-                    <option value={"5"}>5月</option>
-                    <option value={"6"}>6月</option>
-                    <option value={"7"}>7月</option>
-                    <option value={"8"}>8月</option>
-                    <option value={"9"}>9月</option>
-                    <option value={"10"}>10月</option>
-                    <option value={"11"}>11月</option>
-                    <option value={"12"}>12月</option>
-                </select>
-                <button><a href={""}>年間</a></button>
-            </div>
+            <Header/>
         <div className={"calendar"}>
             <FullCalendar
                 plugins={[dayGridPlugin, listPlugin]}

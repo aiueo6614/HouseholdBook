de45ed7569d37297b588225728f54d9126ce1ff8
AMAKARATEBASAKI <cemm1240052@gn.iwasaki.ac.jp>
2025-01-31 09:09:03 +0900

    見た目をいじったまだ中身はない

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -52,52 +52,61 @@ function App() {
     return (
         <div>
             <Header/>
-        <div className={"calendar"}>
-            <FullCalendar
-                plugins={[dayGridPlugin, listPlugin]}
-                initialView="dayGridMonth"
-                locales={[jaLocale]}
-                locale="ja"
-                headerToolbar={{
-                    right: 'dayGridMonth,listMonth',
-                }}
-                events={[
-                    {title: 'event 1', start: time[0]},
-                    {title: 'event 3', start: time[1]},
-                    {title: 'event 2', start: time[0], end: time[2]}
-                ]}
-            />
-            <button onClick={sendData}>データ送信</button>
-            <button onClick={getData}>データ取得</button>
-            {/*取得したテストデータ表示*/}
-            <div>
-                <h2>取得したデータ</h2>
-                <ul>
-                    {transactions.map((tx, index) => (
-                        <li key={index}>{tx.date} - {tx.category} - {tx.description} - {tx.amount}円</li>
-                    ))}
-                </ul>
+            <button className={"save"}>保存</button>
+            <div className={"SecondaryHeader"}>
+                {/*左三角形*/}
+                <div className={"PreviousMonth"}><a href={""}></a></div>
+                {/*右三角形*/}
+                <div className={"NextMonth"}><a href={""}></a></div>
             </div>
-            <div>
-                <h2>取得したデータのカテゴリー別け</h2>
+            <div className={"calendar"}>
+                <FullCalendar
+                    plugins={[dayGridPlugin, listPlugin]}
+                    initialView="dayGridMonth"
+                    locales={[jaLocale]}
+                    locale="ja"
+                    headerToolbar={{
+                        left: '',
+                        center:'',
+                        right: 'dayGridMonth,listMonth',
+                    }}
+                    events={[
+                        {title: 'event 1', start: time[0]},
+                        {title: 'event 3', start: time[1]},
+                        {title: 'event 2', start: time[0], end: time[2]}
+                    ]}
+                />
+                <button onClick={sendData}>データ送信</button>
+                <button onClick={getData}>データ取得</button>
+                {/*取得したテストデータ表示*/}
+                <div>
+                    <h2>取得したデータ</h2>
+                    <ul>
+                        {transactions.map((tx, index) => (
+                            <li key={index}>{tx.date} - {tx.category} - {tx.description} - {tx.amount}円</li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h2>取得したデータのカテゴリー別け</h2>
 
-                {/* フィルタ用のドロップダウン */}
-                <label htmlFor="categoryFilter">カテゴリーで絞り込み:</label>
-                <select id="categoryFilter" value={categoryFilter} onChange={handleFilterChange}>
-                    <option value="all">すべて</option>
-                    <option value="食費">食費</option>
-                    <option value="交通費">交通費</option>
-                    <option value="娯楽">娯楽</option>
-                    {/* 必要に応じてカテゴリを追加 */}
-                </select>
+                    {/* フィルタ用のドロップダウン */}
+                    <label htmlFor="categoryFilter">カテゴリーで絞り込み:</label>
+                    <select id="categoryFilter" value={categoryFilter} onChange={handleFilterChange}>
+                        <option value="all">すべて</option>
+                        <option value="食費">食費</option>
+                        <option value="交通費">交通費</option>
+                        <option value="娯楽">娯楽</option>
+                        {/* 必要に応じてカテゴリを追加 */}
+                    </select>
 
-                <ul>
-                    {filteredTransactions.map((tx, index) => (
-                        <li key={index}>{tx.date} - {tx.category} - {tx.description} - {tx.amount}円</li>
-                    ))}
-                </ul>
+                    <ul>
+                        {filteredTransactions.map((tx, index) => (
+                            <li key={index}>{tx.date} - {tx.category} - {tx.description} - {tx.amount}円</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
         </div>
     );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import styles from './Header.css';
 
 const Header = () => {
     return (
         <div className="header">
             <button><a href="">ホーム</a></button>
             <select name="月選択" className="month">
+                <option value="0">月選択</option>
                 <option value="1">1月</option>
                 <option value="2">2月</option>
                 <option value="3">3月</option>

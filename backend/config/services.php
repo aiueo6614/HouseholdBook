9f008fc07364db2f7ea5dea9da07b344dd24d1ca
aiueo6614 <aiueo6614@gmail.com>
2025-02-03 11:41:49 +0900

    add: ocr

--- backend/config/services.php
+++ backend/config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_email' => env('GOOGLE_CLIENT_EMAIL'),
+        'private_key' => env('GOOGLE_PRIVATE_KEY'),
+        'drive_id' => env('GOOGLE_DRIVE_ID'),
+    ],
+
 ];
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\GoogleDriveController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,10 @@ Route::group(['prefix' => 'transactions'], function () {
     Route::get('/', [TransactionController::class, 'index']);
     Route::post('/', [TransactionController::class, 'store']);
     Route::get('/show/{id}', [TransactionController::class, 'show']);
+});
+
+Route::group(['prefix' => 'drive'], function () {
+    Route::post('/', [GoogleDriveController::class, 'ocr']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
